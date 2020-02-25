b5a2af2917a18ffbcad35761f9cb7a98e3abe6b1
Margarira <margarita.saveleva1@gmail.com>
2020-02-25 10:32:24 +0100

    SQL request to get list of chapters has been changed in order to obtain the number of comments per chapter

--- BlogMVC/Model/Chapter.php
+++ BlogMVC/Model/Chapter.php
@@ -5,12 +5,16 @@ class Chapter extends Model {
 
 	// Renvoie la liste de tous les billets, triés par identifiant décroissant
 	public function getChapters($order, $start, $quantity) {
-	  $sql = 'SELECT chapter_id AS id, DATE_FORMAT(chapter_date, \'%d/%m/%Y à %Hh%i\') AS date,'
-	    . ' chapter_title AS title, chapter_content AS content FROM chapters'
-	    . ' ORDER BY chapter_id' . ' ' . $order . ' LIMIT ' . $quantity . ' OFFSET ' . $start;
+	  $sql = 'SELECT chapters.chapter_id AS id, DATE_FORMAT(chapter_date, \'%d/%m/%Y à %Hh%i\') AS date,'
+	    . ' chapter_title AS title, chapter_content AS content, COUNT(comments.chapter_id) AS numOfComments'
+	    .' FROM comments RIGHT JOIN chapters ON comments.chapter_id = chapters.chapter_id'
+	    . ' GROUP BY chapters.chapter_id ORDER BY chapters.chapter_id'
+	    . ' ' . $order . ' LIMIT ' . $quantity . ' OFFSET ' . $start;
 	  $chapters = $this->executeRequest($sql);
 	  return $chapters;
 	}
+	
+
 
 	public function getChapter($idChapter) {
 	  $sql = 'SELECT chapter_id AS id, chapter_date AS date,'
